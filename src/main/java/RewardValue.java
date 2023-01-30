9e0d32b84f113c604b4c1a2511269648b93df566
josiah <josiah.chapman@gmail.com>
2023-01-29 20:19:04 -0800

    init

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+package main.java;
+
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor that accepts miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Method to get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
